de0cb82e5983e7d2fe4dad9d3fb1633e530e132d
DinhKietVy <116153273+DinhKietVy@users.noreply.github.com>
2024-10-13 18:13:47 +0700

    Delete Source directory

--- Source/Section6Challenge/Section6Challenge.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Section6Challenge.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Section6Challenge, "Section6Challenge" );
--- Source/Section6Challenge/Section6Challenge.h
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "CoreMinimal.h"
-
-#define DRAW_LINE(StartLocation, EndLocation,Color) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation,Color, true, -1.f, 0, 1.f);
